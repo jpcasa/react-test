7ed6a78d00fb781eec0d3df3e860e784fd5181a0
Juan Pablo Casabianca <hola@jpcasabianca.com>
2025-04-11 15:52:26 -0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <img src="/rego.webp" alt="Rego" />
+      <p>This is the output:</p>
+      <span>{'PRINT OUT THE USERNAMES HERE...'}</span>
+    </>
+  )
+}
+
+export default App
---
+++ src/data.js
@@ -0,0 +1,15 @@
+export const people = [
+  ["Jane", "Doe", "jane@doe.com"],
+  ["John", "Smith", "john@smith.com"],
+  ["Jim", "Beam", "jim@beam.com"],
+  ["Jill", "Hill", "jill@hill.com"],
+  ["Jake", "Jones", "jake@jones.com"],
+]
+
+export const weirdPeople = [
+  ["Bob", "Smith", "bob.smith1@email.com"],
+  ["Bob", "Smith", "bob.smith2@email.com"],
+  ["John", "Michaels", "john@john.com"],
+  ["Jill", "Michaels", "mike@mike.com"],
+  ["Jake", "Michaels", "jake@jake.com"],
+]
